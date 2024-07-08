9ecdc8ee3a7a68ee9f0923764b205e5c75f04b89
Quentin Legiemble <q.legiemble@gmail.com>
2024-07-09 01:40:34 +0200

    init mappers

---
+++ src/main/java/com/libra_s/libraS/dtos/mapper/AuthorMapper.java
@@ -0,0 +1,24 @@
+package com.libra_s.libraS.dtos.mapper;
+
+import com.libra_s.libraS.domain.Author;
+import com.libra_s.libraS.dtos.AuthorDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", uses = {})
+public interface AuthorMapper extends EntityMapper<AuthorDto, Author> {
+
+        @Override
+        Author toEntity(AuthorDto dto);
+
+        @Override
+        AuthorDto toDto(Author entity);
+
+        @Override
+        List<Author> toEntity(List<AuthorDto> dtoList);
+
+        @Override
+        List<AuthorDto> toDto(List<Author> entityList);
+}
