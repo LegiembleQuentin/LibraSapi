4badb9b366618f1e103e24567558efb257df2734
Quentin Legiemble <q.legiemble@gmail.com>
2024-07-10 16:42:32 +0200

    correction compatibilité filters JWT et API key

--- src/main/java/com/libra_s/libraS/rest/BookController.java
+++ src/main/java/com/libra_s/libraS/rest/BookController.java
@@ -3,21 +3,17 @@ package com.libra_s.libraS.rest;
 import com.libra_s.libraS.domain.AppUser;
 import com.libra_s.libraS.dtos.BookDto;
 import com.libra_s.libraS.dtos.DiscoverPageDto;
-import com.libra_s.libraS.repository.AppUserRepository;
 import com.libra_s.libraS.service.AppUserService;
 import com.libra_s.libraS.service.BookService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Optional;
 
 @RequestMapping("/api")
 @RestController
@@ -40,15 +36,18 @@ public class BookController {
 
     @GetMapping("/books/discover")
     public ResponseEntity<?> getDiscoverPageInfos() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        try {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            AppUser currentUser = (AppUser) authentication.getPrincipal();
 
-        AppUser currentUser = (AppUser) authentication.getPrincipal();
-
-        if (currentUser != null){
-            DiscoverPageDto result = bookService.getDiscoverPageInfos(currentUser.getId());
-            return ResponseEntity.ok(result);
-        } else {
-            return ResponseEntity.badRequest().body("Must be logged in");
+            if (currentUser != null) {
+                DiscoverPageDto result = bookService.getDiscoverPageInfos(currentUser.getId());
+                return ResponseEntity.ok(result);
+            } else {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Must be logged in");
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Must be logged in");
         }
     }
 }
