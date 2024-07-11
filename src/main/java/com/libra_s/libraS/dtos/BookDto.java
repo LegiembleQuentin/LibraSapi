45bc0ca5ff8ebae57fd3278171ac99f7f13b2349
Quentin Legiemble <q.legiemble@gmail.com>
2024-07-11 13:33:24 +0200

    adapt book dto with users infos

--- src/main/java/com/libra_s/libraS/dtos/BookDto.java
+++ src/main/java/com/libra_s/libraS/dtos/BookDto.java
@@ -1,9 +1,11 @@
 package com.libra_s.libraS.dtos;
 
+import com.libra_s.libraS.domain.enums.UserBookStatus;
 import lombok.Data;
 
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.List;
@@ -28,13 +30,23 @@ public class BookDto {
 
     private BigDecimal note;
 
+    private String imgUrl;
+
 //    private Set<TagDto> tags = new HashSet<>();
 
     private Set<AuthorDto> authors = new HashSet<>();
 
     private Set<BookDto> relatedBooks = new HashSet<>();
 
+    private Set<BookDto> sameAuthorBooks = new HashSet<>();
+
     private LocalDate createdAt;
 
     private LocalDate modifiedAt;
+
+    private UserBookStatus userStatus;
+
+    private int userRating;
+
+    private int userCurrentVolume;
 }
