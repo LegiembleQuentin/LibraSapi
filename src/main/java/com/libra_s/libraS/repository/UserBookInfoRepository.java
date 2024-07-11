a71e87cfc8b9551a7f3e011573226a9dc485f1f8
Quentin Legiemble <q.legiemble@gmail.com>
2024-07-11 14:11:26 +0200

    create UserBookInfoService

--- src/main/java/com/libra_s/libraS/repository/UserBookInfoRepository.java
+++ src/main/java/com/libra_s/libraS/repository/UserBookInfoRepository.java
@@ -4,7 +4,10 @@ import com.libra_s.libraS.domain.UserBookInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserBookInfoRepository extends JpaRepository<UserBookInfo, Long> {
 
+    Optional<UserBookInfo> findByAppUserIdAndBookId(Long userId, Long bookId);
 }
